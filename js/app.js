dad9402eb89fcd2aa3894783987cbd938ae1e1d4
alemalagni <alemalagni@gmail.com>
2025-03-13 15:23:03 +0100

    test: JS script

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
-fetch( "https://flynn.boolean.careers/exercises/api/random/mail" )
-    .then( response => response.json() )
-    .then( data => {
-        for ( let i = 0; i < 10; i++ ) {
+for ( let i = 0; i < 10; i++ ) {
+    fetch( "https://flynn.boolean.careers/exercises/api/random/mail" )
+        .then( response => response.json() )
+        .then( data => {
             console.log(data.response);
-        }
-    });
-    
+        });
+    }
