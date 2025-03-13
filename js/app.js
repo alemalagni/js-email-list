4242ae7af90d24d68e16ee13cb36574ce76d48b7
alemalagni <alemalagni@gmail.com>
2025-03-13 15:28:01 +0100

    feat: add JS script

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
-for ( let i = 0; i < 10; i++ ) {
+// Funzione
+function generaEmail () {
     fetch( "https://flynn.boolean.careers/exercises/api/random/mail" )
         .then( response => response.json() )
         .then( data => {
             console.log(data.response);
         });
-    }
+};
+
+// Script
+const lista = document.getElementById("lista") 
+
+for ( let i = 0; i < 10; i++ ) {
+    lista.innerHTML = `
+    <li><div><span>${generaEmail()}</span></div></li>
+    `
+};
