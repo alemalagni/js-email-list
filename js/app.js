bca1e2ea36fb28731ffdddd82520ccee5934eaab
alemalagni <alemalagni@gmail.com>
2025-03-13 16:23:38 +0100

    feat: add JS button script

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 // Script
 const lista = document.getElementById("lista")
+const button = document.getElementById("button")
 
-for ( let i = 0; i < 10; i++ ) {
-    fetch( "https://flynn.boolean.careers/exercises/api/random/mail" )
-        .then( response => response.json() )
-        .then( data => {
-            lista.innerHTML = lista.innerHTML + `
-            <li><div><span>${data.response}</span></div></li>
-            `
-        });
-};
+button.addEventListener( "click",
+    function() {
+        lista.innerHTML = "";
+        for ( let i = 0; i < 10; i++ ) {
+            fetch( "https://flynn.boolean.careers/exercises/api/random/mail" )
+                .then( response => response.json() )
+                .then( data => {
+                    lista.innerHTML = lista.innerHTML + `
+                    <li><div><span>${data.response}</span></div></li>
+                    `
+                });
+        };
+    }
+);
