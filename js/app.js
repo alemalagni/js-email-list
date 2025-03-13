635278a6ba161d78c3b50fa36bf97b995188ae6e
alemalagni <alemalagni@gmail.com>
2025-03-13 16:02:08 +0100

    fix: JS script

--- js/app.js
+++ js/app.js
@@ -1,20 +1,12 @@
-// Funzione
-function generaEmail(lista) {
+// Script
+const lista = document.getElementById("lista")
+
+for ( let i = 0; i < 10; i++ ) {
     fetch( "https://flynn.boolean.careers/exercises/api/random/mail" )
         .then( response => response.json() )
         .then( data => {
-            lista = lista + `
+            lista.innerHTML = lista.innerHTML + `
             <li><div><span>${data.response}</span></div></li>
             `
-            return lista;
         });
-    
-};
-
-// Script
-const lista = document.getElementById("lista") 
-let stringa = "";
-for ( let i = 0; i < 10; i++ ) {
-    stringa = lista.innerHTML;
-   generaEmail(stringa);
 };
